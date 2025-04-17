6204b2f1dccec04c832e0b069abff8e652731532
Icham Duret <57659587+darkgugu@users.noreply.github.com>
2025-04-17 11:00:24 +0200

    Added modal

--- site/src/components/Header.jsx
+++ site/src/components/Header.jsx
@@ -2,8 +2,30 @@ import { Link } from 'react-router'
 import '../assets/css/Header.css'
 import dice from '../assets/images/dice.png'
 import { Currency } from './Currency'
+import Modal from 'react-modal'
+import { useState } from 'react'
+import { useAuth } from '../context/AuthContext'
 
 export const Header = () => {
+	const customStyles = {
+		content: {
+			top: '50%',
+			left: '50%',
+			right: 'auto',
+			bottom: 'auto',
+			marginRight: '-50%',
+			transform: 'translate(-50%, -50%)',
+		},
+	}
+	const [modalIsOpen, setModalIsOpen] = useState(false)
+
+	function openModal() {
+		setModalIsOpen(true)
+	}
+	function closeModal() {
+		setModalIsOpen(false)
+	}
+
 	return (
 		<div className="Header">
 			<Link to="/">
@@ -16,9 +38,21 @@ export const Header = () => {
 				<Currency type="diamond" />
 				<Currency type="dollar" />
 				<button id="shop">Boutique</button>
-				<button id="login">Connexion</button>
-				<button id="register">Inscription</button>
+				<button id="login" onClick={openModal}>
+					Connexion
+				</button>
+				<button id="register" onClick={openModal}>
+					Inscription
+				</button>
 			</div>
+			<Modal
+				isOpen={modalIsOpen}
+				onRequestClose={closeModal}
+				style={customStyles}
+				contentLabel="Example Modal"
+			>
+				<button onClick={closeModal}>close</button>
+			</Modal>
 		</div>
 	)
 }
