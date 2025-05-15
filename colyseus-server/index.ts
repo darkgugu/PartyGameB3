29129ae54ccee7291db4b4aa041b2d1deab6c2e3
Icham Duret <57659587+darkgugu@users.noreply.github.com>
2025-05-15 12:42:23 +0200

    Updates for Railway deployment

--- colyseus-server/index.ts
+++ colyseus-server/index.ts
@@ -4,11 +4,11 @@ import express from "express";
 import { MyRoom } from "./rooms/MyRoom";
 
 const app = express();
-const port = 2567;
+const port = Number(process.env.PORT) || 2567;
 
 const server = createServer(app);
 const gameServer = new Server({ server });
 
 gameServer.define("labyrinth", MyRoom);
 gameServer.listen(port);
-console.log(`Colyseus listening on ws://localhost:${port}`);
+console.log(`Colyseus listening on ws://0.0.0.0:${port}`);
