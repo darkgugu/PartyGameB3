61085ebe2c80a5840453c176ea993172edc9ece8
Icham Duret <57659587+darkgugu@users.noreply.github.com>
2025-07-21 07:01:54 +0200

    Fixes tries

--- site/src/components/BabylonScene.jsx
+++ site/src/components/BabylonScene.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react'
+import { useEffect, useRef } from 'react'
 import * as BABYLON from '@babylonjs/core'
 import { useBabylon } from '../context/BabylonProvider'
 import '@babylonjs/loaders'
